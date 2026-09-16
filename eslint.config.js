88c964f7fbf63c3d4888e11c48fc291ed2bf8ab6
tdd3vlp <tdd3vlp@gmail.com>
2026-09-16 12:37:45 +0300

    init: add eslint with typescript-eslint and unicorn

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import unicorn from "eslint-plugin-unicorn";
+import globals from "globals";
+
+export default defineConfig(
+  globalIgnores(["dist/"]),
+  js.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  unicorn.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    linterOptions: {
+      noInlineConfig: true,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+    },
+  },
+  {
+    files: ["**/*.js"],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+);
